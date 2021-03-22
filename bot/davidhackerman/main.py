a7f1f545bcf27d84f10c602643d998c334a04f59
davidracovan <74030080+davidracovan@users.noreply.github.com>
2021-03-22 17:30:33 -0400

    fix main.py

--- bot/davidhackerman/main.py
+++ bot/davidhackerman/main.py
@@ -10,10 +10,10 @@ from bot.cogs.fun import Fun
 from bot.cogs.help import HelpCommand
 from bot.cogs.info import Info
 from bot.cogs.search import Search
-from bot.games.tictactoe import TicTacToe
+from bot.cogs.games import Games
 from bot.davidhackerman.cogs.economy import Economy
 from bot.davidhackerman.cogs.links import Links
-from bot.cogs.punishments import Punishments
+from bot.cogs.moderation import Moderation
 from bot.davidhackerman.cogs.bottling import Bottling
 
 # https://discord.com/api/oauth2/authorize?client_id=796805491186597968&permissions=2147483639&scope=bot
@@ -29,8 +29,8 @@ def start():
     bot.add_cog(Search(bot))
     bot.add_cog(Links(bot))
     bot.add_cog(Economy(bot))
-    bot.add_cog(Punishments(bot))
-    bot.add_cog(TicTacToe(bot))
+    bot.add_cog(Moderation(bot))
+    bot.add_cog(Games(bot))
     bot.add_cog(Bottling(bot))
     dotenv.load_dotenv()
     bot.AZURE_KEY = os.environ['AZURE_KEY']
