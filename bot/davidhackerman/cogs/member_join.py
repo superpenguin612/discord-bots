c80d04e7432fbfc309474a549dca1c546cd3f5af
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2021-06-09 12:15:24 -0400

    fix member_join.py

--- bot/davidhackerman/cogs/member_join.py
+++ bot/davidhackerman/cogs/member_join.py
@@ -9,10 +9,14 @@ class MemberJoin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member) -> None:
+        print("bru")
         guild = member.guild
         if guild.id == 852039928589713429:
             role = guild.get_role(852047902024400957)
-            member.add_roles(role)
+            try:
+                await member.add_roles(role)
+            except:
+                pass
 
 
 def setup(bot: commands.Bot):
